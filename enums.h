ed23654de886bedec2537f8d4a10f611c4510eb6
alexjrenaut <ajrenaut@crimson.ua.edu>
2019-03-14 14:00:43 -0500

    completing earlier commit

--- enums.h
+++ enums.h
@@ -1,3 +1,7 @@
+/* Enums are used in the place of integers throughout the program,
+ * to handle switch and other conditional statements.
+ */
+
 enum { EMPTY = 0, VS_AI = 1, VS_PLAYER = 2,
 	   RED = 3, BLUE = 4, P1_WIN = 5, P2_WIN = 6,
 	   RUNNING = 7, PLAYER = 8, AI = 9,
